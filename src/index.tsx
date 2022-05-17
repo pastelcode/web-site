46c5f959c93677500c2141bc0e5e411f3f99fadf
Pastel <pastel.dev.gt@gmail.com>
2022-05-17 07:21:10 -0600

    Cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
-import { ColorModeScript } from "@chakra-ui/react"
-import * as React from "react"
-import * as ReactDOM from "react-dom/client"
-import { App } from "./App"
-import reportWebVitals from "./reportWebVitals"
-import * as serviceWorker from "./serviceWorker"
+import { ColorModeScript } from '@chakra-ui/react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './App'
+// import reportWebVitals from './reportWebVitals'
+import * as serviceWorker from './serviceWorker'
 
-const container = document.getElementById("root")
-const root = ReactDOM.createRoot(container)
+const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <React.StrictMode>
-    <ColorModeScript />
+  <StrictMode>
+    <ColorModeScript initialColorMode="system" />
     <App />
-  </React.StrictMode>,
+  </StrictMode>
 )
 
 // If you want your app to work offline and load faster, you can change
@@ -23,4 +22,4 @@ serviceWorker.unregister()
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+// reportWebVitals()
