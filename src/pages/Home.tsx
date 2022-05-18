b15875e402b48622db75cd0aa61197163dd1d5fd
Pastel <pastel.dev.gt@gmail.com>
2022-05-17 21:56:42 -0600

    Add CTA and reasons to choose us to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
+import { VStack } from '@chakra-ui/react'
+
+import Introduction from '../components/home/Introduction'
+import WhyToChooseUs from '../components/home/WhyToChooseUs'
+
 const Home = (): JSX.Element => {
-  return <></>
+  return (
+    <VStack>
+      <Introduction />
+      <WhyToChooseUs />
+    </VStack>
+  )
 }
 
 export default Home
