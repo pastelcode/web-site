c0ab20ca70d7376eae4fa285f472ec3074885005
Pastel <pastel.dev.gt@gmail.com>
2022-05-19 13:29:38 -0600

    Finish footer design

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,14 @@
-import { Box, Image, VStack } from '@chakra-ui/react'
+import {
+  Box,
+  HStack,
+  Image,
+  VStack,
+  LinkBox,
+  LinkOverlay,
+  Icon,
+  Heading,
+} from '@chakra-ui/react'
+import { socialMedia } from '../../config/brandInformation'
 
 const Footer = (): JSX.Element => {
   return (
@@ -7,6 +17,19 @@ const Footer = (): JSX.Element => {
         <Image src="/pastel.svg" alt="Pastel" height={24} />
         <Box height={1} />
         <Image src="/pastel-letters.svg" alt="Pastel" height={7} />
+        <Box height={6} />
+        {socialMedia.map(({ url, name, icon }) => (
+          <LinkBox key={url}>
+            <HStack>
+              <Icon as={icon} boxSize={6} />
+              <Heading as="h6" size="md">
+                <LinkOverlay href={url} isExternal>
+                  {name}
+                </LinkOverlay>
+              </Heading>
+            </HStack>
+          </LinkBox>
+        ))}
       </VStack>
     </Box>
   )
