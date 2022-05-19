3aed7f299d2d105d7877480d765ffd39073bbaa0
Pastel <pastel.dev.gt@gmail.com>
2022-05-19 12:25:07 -0600

    Write brand information

---
+++ src/config/brandInformation.ts
@@ -0,0 +1,11 @@
+interface Information {
+  github: string
+  instagram?: string
+  youtube?: string
+}
+
+const brandInformation: Information = {
+  github: 'https://github.com/pastelcode',
+}
+
+export default brandInformation
