7c00c153d81f3e6e5a5249a2ed59ebc4311baeb3
Pastel <pastel.dev.gt@gmail.com>
2022-05-19 19:18:31 -0600

    Stylize nav bar
    Add logo
    Add background color when page not in top

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,5 +1,23 @@
-import { Box } from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
+import { Box, HStack, Image } from '@chakra-ui/react'
+
+import { brandLetters, brandLogo } from '../../config/brandInformation'
 const NavBar = () => {
+  const [scrollPosition, setScrollPosition] = useState<number>(0)
+
+  const handleScroll = () => {
+    const position = window.scrollY
+    setScrollPosition(position)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true })
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
     <Box
       as="nav"
@@ -7,10 +25,15 @@ const NavBar = () => {
       top={0}
       width="100%"
       transform="translateZ(0)"
-      background="red"
       zIndex="sticky"
+      background={scrollPosition >= 75 ? 'blackAlpha.800' : 'transparent'}
     >
-      NavBar
+      <HStack>
+        <HStack height={16}>
+          <Image src={brandLogo} alt="Pastel" height="60%" />
+          <Image src={brandLetters} alt="Pastel" height="30%" />
+        </HStack>
+      </HStack>
     </Box>
   )
 }
