2269a913d49afac687fe401a068c3a15e12aeae3
Pastel <pastel.dev.gt@gmail.com>
2022-05-21 00:05:53 -0600

    Delete unnecesary `reference` field in services

--- src/config/brandInformation.ts
+++ src/config/brandInformation.ts
@@ -20,7 +20,6 @@ export const socialMedia: SocialMedia[] = [
 interface Service {
   title: string
   description: string
-  reference: string
 }
 
 export const ourServices: Service[] = [
@@ -28,19 +27,16 @@ export const ourServices: Service[] = [
     title: 'Landing pages',
     description:
       'Dale una bienvenida acogedora a tu cliente y permite que conozca lo que haces y contacte contigo',
-    reference: 'landing',
   },
   {
     title: 'Tiendas en línea',
     description:
       'Expande los límites de tu negocio y empieza a vender a través de Internet y de forma remota',
-    reference: 'store',
   },
   {
     title: 'Aplicaciones móviles',
     description:
       'Mueve tu idea a la comodidad de la palma de la mano, en cualquier plataforma y en cualquier dispositivo',
-    reference: 'mobile',
   },
 ]
 
