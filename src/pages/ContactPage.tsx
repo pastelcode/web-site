dc3cde578c82cf03b8757a0ef6f397874f5e3ac1
Pastel <pastel.dev.gt@gmail.com>
2022-05-23 13:41:07 -0600

    Add contact page to routes

---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,11 @@
+import { Heading, VStack } from '@chakra-ui/react'
+
+const ContactPage = (): JSX.Element => {
+  return (
+    <VStack>
+      <Heading>Contacto</Heading>
+    </VStack>
+  )
+}
+
+export default ContactPage
