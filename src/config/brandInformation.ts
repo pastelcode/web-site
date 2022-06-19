de5670b227e510116369aca7e4e0de977978e5c1
Pastel <pastel.dev.gt@gmail.com>
2022-06-19 15:09:49 -0600

    Add links to YouTube and Instagram

--- src/config/brandInformation.ts
+++ src/config/brandInformation.ts
@@ -1,5 +1,5 @@
 import { BiPaintRoll, BiExtension, BiHeart } from 'react-icons/bi'
-import { BsGithub } from 'react-icons/bs'
+import { BsGithub, BsInstagram, BsYoutube } from 'react-icons/bs'
 import { IconType } from 'react-icons/lib'
 
 interface SocialMedia {
@@ -14,6 +14,16 @@ export const socialMedia: SocialMedia[] = [
     url: 'https://github.com/pastelcode',
     name: 'Github',
     icon: BsGithub,
+  },
+  {
+    url: 'https://www.instagram.com/pastelcode.dev',
+    name: 'Instagram',
+    icon: BsInstagram,
+  },
+  {
+    url: 'https://www.youtube.com/channel/UCOsYvHAtI6xtPcN3NORiU2Q',
+    name: 'YouTube',
+    icon: BsYoutube,
   },
 ]
 
